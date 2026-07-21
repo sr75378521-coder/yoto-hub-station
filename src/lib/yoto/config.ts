bbf180735606c2dfe357c4ced53998f325c1439a
sr75378521-coder <sr75378521@gmail.com>
2026-07-21 12:40:12 -0600

    Add 'family:devices:view' to YOTO_SCOPES

--- src/lib/yoto/config.ts
+++ src/lib/yoto/config.ts
@@ -5,5 +5,5 @@
 export const YOTO_AUTH_BASE = "https://login.yotoplay.com";
 export const YOTO_API_BASE = "https://api.yotoplay.com";
 export const YOTO_AUDIENCE = "https://api.yotoplay.com";
-export const YOTO_SCOPES = "openid profile offline_access";
+export const YOTO_SCOPES = "openid profile offline_access family:devices:view";
 export const YOTO_CALLBACK_PATH = "/api/yoto/callback";
